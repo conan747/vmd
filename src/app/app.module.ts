6ea008176f589dde9451ef507dda80ec79a343b3
cos cloudtop <conan747@users.noreply.github.com>
2024-10-08 19:59:47 +0000

    Trying theming.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [provideHttpClient(withInterceptorsFromDi())],
+  imports: [BrowserModule,
+    MatButtonModule,
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
